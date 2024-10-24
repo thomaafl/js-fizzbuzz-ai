c08e86b54945de567deffe0477772c09de6da6da
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-10-24 15:07:01 +0200

    fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,22 @@
+/* eslint-disable prefer-const */
+/* eslint-disable prettier/prettier */
 // TODO: Write your function in this file
 
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+function fizzBuzz(lower, upper) {
+    let result = [];
+    for (let i = lower; i <= upper; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+module.exports = fizzBuzz;
